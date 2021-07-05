ff49de832d121a2be6908606148865aba5abb6a8
Amir Worms <amirworms@gmail.com>
2021-07-05 12:12:10 +0200

    Ajout de sinks

--- quickstart/src/main/scala/org/myorg/quickstart/StreamingJob.scala
+++ quickstart/src/main/scala/org/myorg/quickstart/StreamingJob.scala
@@ -43,18 +43,22 @@ import java.util.concurrent.TimeUnit
 object StreamingJob {
 
   def main(args: Array[String]): Unit = {
-    // set up the streaming execution environment
 
+    // Parameters
     val window = Time.of(60, TimeUnit.SECONDS)
-    val outputPath = "output_clicks.txt"
+    val outputPath = "/Users/amirworms/Documents/Streaming/kafka-fraud-detection/quickstart/snapshots"
 
+    val outputFile = "output_clicks.txt"
+
+    // set up the streaming execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
     val properties = new Properties()
     properties.setProperty("bootstrap.servers", "localhost:9092")
     properties.setProperty("group.id", "test")
 
-    val displaysKafkaConsumer = new FlinkKafkaConsumer[String]("displays", new SimpleStringSchema(), properties)
+    // Consumers
+    val displaysKafkaConsumer = new FlinkKafkaConsumer[String]("displays", new SimpleStringSchema(), properties) // Possible to use KafkaDeserializationSchema
     val clicksKafkaConsumer = new FlinkKafkaConsumer[String]("clicks", new SimpleStringSchema(), properties)
 
     val displayStream
@@ -62,23 +66,54 @@ object StreamingJob {
     val clickStream
     = env.addSource(clicksKafkaConsumer)
 
-    //val wordCounts = countWords(lines, stopWords, window)
-
-    /*wordCounts
-      .map(_.toString)
-      .addSink(kafkaProducer)
-    */
-
+    // Print
     clickStream.print()
     displayStream.print()
 
+    /* Sink to Kafka producer
+    // 1st idea : https://sparkbyexamples.com/kafka/apache-kafka-consumer-producer-in-scala/?fbclid=IwAR2F8hs69ka7JjjMdqeVx5p35sVf66jQoQqnp-qLft998NbR9lGMl4TntkU
+    // 2nd idea : https://ci.apache.org/projects/flink/flink-docs-release-1.13/docs/connectors/datastream/kafka/
+    val properties_producer = new Properties
+    properties_producer.setProperty("bootstrap.servers", "localhost:9092")
+
+    val topic = "fraud_clicks"
+    val fraudClicksProducer = new FlinkKafkaProducer[String](
+      topic,                  // target topic
+      new KafkaSerializationSchema[String]() {
+        override def serialize(element: String, aLong: lang.Long): ProducerRecord[Array[Byte], Array[Byte]] = new ProducerRecord[Array[Byte], Array[Byte]](topic, element.getBytes(StandardCharsets.UTF_8))
+      },    // serialization schema #https://stackoverflow.com/questions/58963483/scala-string-sink-for-flinkkafkaproducer-extending-kafkaserializationschema
+      properties_producer,                  // producer config
+      FlinkKafkaProducer.Semantic.AT_LEAST_ONCE) // fault-tolerance
+
+    clickStream.addSink(fraudClicksProducer)
+
+    //fraudClicksProducer.send(record)
+
+    "Error while fetching metadata with correlation id"
+    -> Test that the "kafka" and "mongo" domain names resolve to the correct brokers (e.g. with kafka-manager ?), and that the port is open to the machine that the monitor is running on.
+
+    */
+    // https://www.codota.com/code/java/classes/org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink
+    import org.apache.kafka.clients.consumer.ConsumerRecord
+    import org.apache.kafka.clients.consumer.ConsumerRecords
+    import java.time.Duration
+    while ( {
+      true
+    }) {
+      val records = clicksKafkaConsumer.poll(Duration.ofMillis(100)) // Fetcher has to be used instead : https://ci.apache.org/projects/flink/flink-docs-release-1.12/api/java/org/apache/flink/streaming/connectors/kafka/FlinkKafkaConsumer.html
+      import scala.collection.JavaConversions._
+      for (record <- records) {
+        System.out.printf("offset = %d, key = %s, value = %s%n", record.offset, record.key, record.value)
+      }
+    }
+
+    // Sink to a file
     // https://ci.apache.org/projects/flink/flink-docs-release-1.13/docs/connectors/datastream/streamfile_sink/
     val sink: StreamingFileSink[String] = StreamingFileSink
       .forRowFormat(new Path(outputPath), new SimpleStringEncoder[String]("UTF-8"))
       .build()
 
-    clickStream.addSink(sink)
-    clickStream.sinkTo()
+    clickStream.addSink(sink).name("Fraudulent Clicks Sink")
 
     // execute program
     env.execute("Flink Streaming Scala API Skeleton")
